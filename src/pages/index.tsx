84f54a3f34dce41260c3843354a130d7a95aa54b
imam16 <muhammadsambo@live.com>
2025-01-28 15:10:05 -0500

    Fixes to shoppingcart, state and stuckForms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,14 @@
 //import Gallery from "./components/gallery"
 import Todos from "./components/todos"
 import RecipeList from "./qcomps/recipes"
-import Gallery from "./qcomps/gallery_props"
+//import Gallery from "./qcomps/gallery_props"
 import List from "./qcomps/list_keys_id"
+import Gallery from "./qcomps/state"
+import Form from "./qcomps/stuckForm"
+import ShoppingCart from "./qcomps/shoppingCart"
 
 export default function Home() {
   return (
-    <RecipeList />
+    <ShoppingCart />
   )
 }
